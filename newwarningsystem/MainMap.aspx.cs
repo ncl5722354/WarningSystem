bbf07f13dae10c9a0359e9bbef29f5c53351289f
ncl5722354 <897117296?@qq.com>
2019-05-11 01:09:19 +0800

    52156

---
+++ newwarningsystem/MainMap.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace newwarningsystem
+{
+    public partial class MainMap : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Imagemap_DataBinding(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
